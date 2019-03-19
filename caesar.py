3c078e64182db1928b775b79ba0da2a9e224f20b
Ahmed Shaikh <ahmedshaikh@Ahmeds-MacBook-Pro.local>
2019-03-19 13:34:02 -0230

    modify caesar

--- caesar.py
+++ caesar.py
@@ -1,21 +1,32 @@
+# CAESAR CIPHER
+
+# function to cipher plaintext and decipher ciphered text
 def de_cipher(ip_text, op_text, cl):
     for i in ip_text:
+
+        # cl short for type, e is encrypt, other alphabets are decrypt
         if cl == 'e':
             op_text.append( alpha[(alpha.index(i) + key)%26] )
         else:
             op_text.append( alpha[(alpha.index(i) - key)%26] )
     return op_text
 
+
+
 alpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 plaintext = input("Enter the plaintext.  ").upper()
 ciphertext = deciphertext = []
 
+
+# do while emulation to enforce appropriate key input
 while True:
     key = int(input("Enter a key value between 1 and 25 (inclusive).  "))
     if (key == 0 or key == 26):
+        # shifting alphabets by 0 or 26 does not beneficially affect plaintext
         print("Cipher text for this key will be same as plaintext. Enter key again.")
     else:
         break
+
 
 print("Ciphertext:  ", ''.join(de_cipher(plaintext, ciphertext, 'e')))
 print("Deciphertext:  ", ''.join(de_cipher(ciphertext, deciphertext, 'd')))
