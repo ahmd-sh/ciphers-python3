93894fb5c913762e742c868c28ebd933356492a8
Ahmed Shaikh <42516500+ahmd-sh@users.noreply.github.com>
2024-01-13 20:26:45 -0500

    Update rsa.py
    minor enhancements

--- rsa.py
+++ rsa.py
@@ -1,5 +1,6 @@
 from math import gcd
 
+
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
@@ -7,23 +8,30 @@ def egcd(a, b):
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
+
 def modinv(a, m):
-    g, x, y = egcd(a, m)
+    g, x, _ = egcd(a, m)
     if g != 1:
-        raise Exception('modular inverse does not exist')
+        raise Exception("modular inverse does not exist")
     else:
         return x % m
 
+
 def is_prime(num):
-    if num < 2: 
-         return False
-    if num % 2 == 0:             
-         return num == 2
-    k = 3
-    while k*k <= num:
-         if num % k == 0:
-             return False
-         k += 2
+    # Optimzation: early return for small numbers
+    if num < 2:
+        return False
+    if num < 4:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    divisor = 5
+    while divisor * divisor <= num:
+        if num % divisor == 0 or num % (divisor + 2) == 0:
+            return False
+        divisor += 6
+
     return True
 
 
@@ -32,40 +40,58 @@ alpha = list("^ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 while True:
     second_prime = 4
     first_prime = int(input("Enter first prime number.  "))
+
     while is_prime(first_prime):
         second_prime = int(input("Enter second prime number.  "))
         if is_prime(second_prime):
             break
         print("Not a prime number.")
-    if first_prime == second_prime:
-        print("Numbers should not be equal.")
-    else:
-        if is_prime(second_prime):
-            break
+
     if second_prime == 4:
         print("Not a prime number.")
+    else:
+        if first_prime == second_prime:
+            print("Numbers should not be equal.")
+        else:
+            if is_prime(second_prime):
+                break
 
 
 product_of_primes = first_prime * second_prime
-totient = (first_prime-1) * (second_prime-1)
+totient = (first_prime - 1) * (second_prime - 1)
 
 while True:
-    e = int(input("Enter 'e'.  "))
-    if e in range(2, totient-1) and gcd(e,totient) == 1:
+    public_exponent = int(input("Enter public exponent 'e'.  "))
+
+    # 'e' should be coprime to the totient and within the range [2, totient-1]
+    if public_exponent in range(2, totient - 1) and gcd(public_exponent, totient) == 1:
         break
-    print("Invalid 'e' value. Enter 'e' value greater than 1 and less than", totient)
+    print(
+        "Invalid public exponent value. Enter public exponent value greater than 1 and less than",
+        totient,
+    )
 
-d = modinv(e, totient)
+d = modinv(public_exponent, totient)
 
-message = alpha.index(input("Enter character.  ").upper())
+while True:
+    character_input = input("Enter a single character: ").upper()
+
+    if len(character_input) == 1 and character_input in alpha:
+        message = alpha.index(character_input)
+        break
+    else:
+        print(
+            "Invalid input. Please enter a single character from the list:",
+            "".join(alpha),
+        )
 
 bob_signature = (message**d) % product_of_primes
 print("Bob's signature:", bob_signature)
 
-alice_signature = (bob_signature**e) % product_of_primes
+alice_signature = (bob_signature**public_exponent) % product_of_primes
 print("Alice's signature:", alice_signature, "   which is:", alpha[alice_signature])
 
-#verification
+# Verification
 if alice_signature == message:
     print("Valid Signature.")
 else:
